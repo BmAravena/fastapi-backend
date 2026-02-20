6843246e4c43d5c523bb059a46bf1c3529355ede
Benjamin <benjaminaravena999@gmail.com>
2026-02-20 17:20:19 -0300

    fixed error

--- routes/users.py
+++ routes/users.py
@@ -17,15 +17,15 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = user_crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
-    return user_crud.crud.create_user(db=db, user=user)
+    return user_crud.create_user(db=db, user=user)
 
 
 @router.post("/admin/", response_model=schemas.UserOut)
 def create_user(user: schemas.UserAdmin, db: Session = Depends(get_db)):
-    db_user = user_crud.crud.get_user_by_email(db, email=user.email)
+    db_user = user_crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
-    return user_crud.crud.create_admin(db=db, user=user)
+    return user_crud.create_admin(db=db, user=user)
 
 
 @router.post("/token/", response_model=schemas.Token)
